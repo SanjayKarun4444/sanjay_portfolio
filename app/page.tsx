fd94f874c25b0d257a03f844d8891b71ee4f9a78
Personsk <112571152+Personsk@users.noreply.github.com>
2024-04-06 13:48:48 -0400

    dknoe for tpday this is so annoying

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import { Metadata } from "next";
 import "./globals.css";
 
+
 export const metadata: Metadata = {
   title: "Sanjay Karunamoorthy | My Portfolio",
   description: "Sanjay is a 3rd year Student at the University of Virginia.",
@@ -12,7 +13,7 @@ const Home = () => {
     <div className="flex justify-center items-center h-screen">
       <section className="text-center">
         <Image src="/underConstruction.jpeg" alt="Construction" width={400} height={400} />
-        <div className="space-y-6">
+        <div className="flex justify-center">
           <p>Page is Under Construction :)</p>
         </div>
       </section>
