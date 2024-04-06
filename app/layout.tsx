7231ce1ffa28f4f0aeb36f6d7c2ceb5eec518db3
Personsk <112571152+Personsk@users.noreply.github.com>
2024-04-06 13:10:06 -0400

    setup but not getting css to work

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,28 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import type { Metadata } from "next";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: 'Sanjay | Personal Portfolio',
-  description: 'Sanjay is a 3rd year Student at the University of Virginia.',
-}
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Personal Portfolio",
+    default: "Sanjay Karunamoorthy",
+  },
+  description: "Sanjay is a 3rd year Student at the University of Virginia.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       {/* bg-slate-300 for bluish background bg-gray-50 is for gray */}
-      <body className={`${inter.className} bg-slate-300 text-gray-`}> 
-        {children}</body>
+      <body className={inter.className}>
+        <main className="mx-auto max-w-3xl px-3 py-10 ">{children}</main>
+      </body>
     </html>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import Image from 'next/image'
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+  title: "Sanjay Karunamoorthy | My Portfolio",
+  description: "Sanjay is a 3rd year Student at the University of Virginia.",
+}
+
+//  bg-[url('/img.jpg')] bg-cover bg-center bg-no-repeat px-1 py-8
 
 export default function Home() {
   return (
-    <main className="">
-  
-    </main>
-
+    <section className="bg-black">
+      Front Page
+    </section>
   )
 }
