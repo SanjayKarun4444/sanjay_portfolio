2a91e8b0fa7e2cb8d6f6a19c5f156d1cfd27b163
SanjayKarun4444 <sanjaykarun@gmail.com>
2024-04-13 22:39:18 -0400

    asdfasdf

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,5 @@
 import Link from "next/link"
-
-
-export default function Navbar(){
+export default function navbar(){
     return <header className = "sticky top-0 bg-background">
         <div className="max-w-3xl mx-auto flex flex-wrap justify-between gap-3 px-3 py-4">
             <nav className="space-x-4 font-medium">
