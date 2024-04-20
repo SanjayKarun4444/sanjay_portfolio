7a05638f49aa8d6ba5a0c841096a49997bea8b86
Personsk <112571152+Personsk@users.noreply.github.com>
2024-04-20 10:43:36 -0400

    asklfsfagain

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,34 +1,39 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { OpenAIStream, StreamingTextResponse } from "ai";
-import { ChatCompletionMessageParam } from "ai/prompts";
+import {OpenAIStream, StreamingTextResponse} from "ai"
+import {ChatCompletionMessageParam} from  "ai/prompts";
 import OpenAI from "openai";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
-    try {
-        const body = req.body;
+
+
+export async function POST(req: Request){
+    try{
+        const body = await req.json();
         const messages = body.messages;
 
         const openai = new OpenAI();
 
-        const systemMessage: ChatCompletionMessageParam = {
+        const systemMessage: ChatCompletionMessageParam={
             role: "system",
-            content:
+            content: 
                 "You are a sarcasm bot. You answer use questions in a sarcastic way.",
         };
 
         const response = await openai.chat.completions.create({
-            model: "gpt-3.5-turbo",
+            model:"gpt-3.5-turbo",
             stream: true,
             messages: [systemMessage, ...messages],
         });
 
         const stream = OpenAIStream(response);
-        // Respond with the stream directly
-        res.send(new StreamingTextResponse(stream));
+        return new StreamingTextResponse(stream);
+
 
     } catch (error) {
-        // Handle errors properly
-        console.log(error);
-        res.status(500).json({ error: "Internal server error" });
+        return Response.json({error: "Internal server error"}, {status: 500})
+        // console.log(error);
+        // return {
+        //     statusCode: 500,
+        //     body: JSON.stringify({ error: "Internal server error" })
+        // };
     }
+
 }
