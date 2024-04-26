f6dd53a3bb6510a29ea4870bcf66dcf79808da49
SanjayKarun4444 <sanjaykarun@gmail.com>
2024-04-25 21:42:24 -0400

    asdcsacsacd

---
+++ src/AboutMe/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AboutMe from "../components/aboutme";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <AboutMe />
+    </div>
+  );
+};
+
+export default AboutPage;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,23 @@
-import Link from "next/link"
-import ThemeToggle from "./ThemeToggle"
-export default function navbar(){
-    return <header className = "sticky top-0 bg-background">
-        <div className="max-w-3xl mx-auto flex flex-wrap justify-between gap-3 px-3 py-4">
-            <nav className="space-x-4 font-medium">
-                <Link href="/">home</Link>
-                <Link href="/about">about me</Link>
-                <Link href="/projects">projects</Link>
-            </nav>
+// components/navbar.tsx
 
-            <div>
-                <ThemeToggle />
-            </div>
+import Link from "next/link";
+import ThemeToggle from "./ThemeToggle";
 
+const Navbar: React.FC = () => {
+  return (
+    <header className="sticky top-0 bg-background">
+      <div className="max-w-3xl mx-auto flex flex-wrap justify-between gap-3 px-3 py-4">
+        <nav className="space-x-4 font-medium">
+          <Link href="/">Home</Link>
+          <Link href="/AboutMe">About Me</Link> {/* Make sure casing matches */}
+          <Link href="/projects">Projects</Link>
+        </nav>
+        <div>
+          <ThemeToggle />
         </div>
-
-
-
+      </div>
     </header>
-}
+  );
+};
+
+export default Navbar;
