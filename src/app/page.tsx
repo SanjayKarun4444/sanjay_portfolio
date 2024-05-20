d847127b8045b0c2af74d5989436234fbd183f79
SanjayKarun4444 <sanjaykarun@gmail.com>
2024-05-20 13:21:49 -0400

    sdfsf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -180,7 +180,7 @@ const Home = () => {
             <div className="shadow-md p-6 rounded-lg">
               <h2 className="text-center text-lg font-semibold">Web Development Intern</h2>
               <p className="text-center text-white-600">SS Technology Consultants, Reston, VA</p>
-              <p className="text-center text-white-500 mt-2">May 2023 - December 2023</p>
+              <p className="text-center text-white-500 mt-2">May 2023 - August 2023</p>
               <br />
               <ul className="text-white-500 list-disc">
                 <li>Led the transformation of the company website utilizing React, adhering to MVC architecture and Scrum methodology; collaborated closely with senior team members for mentorship and guidance to ensure successful project completion</li>
