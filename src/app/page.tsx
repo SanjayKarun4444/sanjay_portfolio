8557533ddf754be72bc1861fa161d1917cc3ae0c
SanjayKarun4444 <sanjaykarun@gmail.com>
2024-06-07 10:50:00 -0400

    through to for

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -158,7 +158,7 @@ const Home = () => {
               <p className="text-center text-white-500 mt-2">May 2024 – Present</p>
               <br />
               <ul className="text-white-500 list-disc">
-                <li>Preprocessing data, vectorizing for semantic search through RAG.</li>
+                <li>Preprocessing data, vectorizing for semantic search for RAG.</li>
               </ul>
             </div>
 
